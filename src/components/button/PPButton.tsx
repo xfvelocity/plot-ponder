a1fa688644304b9a3e5adc8a29f83a8c8e203b63
Alex Long <alexlong2001@outlook.com>
2023-08-26 10:24:53 +0100

    added fonts & button

--- src/components/button/PPButton.tsx
+++ src/components/button/PPButton.tsx
@@ -1,9 +1,40 @@
+// ** Styles **
+import "./ppButton.scss";
+
+// ** Components **
+import PPLoading from "../loading/PPLoading";
+
+// ** Types **
 interface Props {
   text: string;
+  backgroundColour?: string;
+  textColour?: string;
+  width?: number | string;
+  disabled?: boolean;
+  loading?: boolean;
 }
 
-const PPButton = ({ text }: Props) => {
-  return <button>{text}</button>;
+const PPButton = ({
+  text,
+  backgroundColour = "blue-darken-3",
+  textColour = "white",
+  width = 200,
+  disabled = false,
+  loading = false,
+}: Props) => {
+  return (
+    <button
+      className={`pp-button pp-bg-${backgroundColour} pp-text-colour-${textColour} ${
+        disabled || (loading && "pp-button-disabled")
+      }`}
+      style={{ width }}
+      disabled={disabled || loading}
+    >
+      {text}
+
+      {loading && <PPLoading colour="white" size={12} />}
+    </button>
+  );
 };
 
 export default PPButton;
