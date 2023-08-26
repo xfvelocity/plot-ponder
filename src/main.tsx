9c5ee362cbd5d714900787f35bcc88da3c00115a
Alex Long <alexlong2001@outlook.com>
2023-08-26 09:35:31 +0100

    added tailwindcss & sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// ** Styles **
+import "./assets/styles/index.scss";
+
+// ** Components **
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
