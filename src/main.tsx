264460ef882be96dc2c3801269068ee54c99c120
David <davidcorreia.uk@gmail.com>
2023-09-02 17:50:55 +0100

    made chip component

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./assets/styles/index.scss";
 
 // ** Components **
 import App from "./App.tsx";
+//import Chip from "./components/chip/chip.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
