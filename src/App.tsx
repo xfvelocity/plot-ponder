cb742b5d850477df3cd5735ee0e380067579b2c9
Alex Long <alexlong2001@outlook.com>
2023-09-02 15:39:27 +0100

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import PPSelect from "./components/select/PPSelect";
+import { useState } from "react";
+import PPSelect from "./components/basic/select/PPSelect";
 
 const App = () => {
+  const [pizza, setPizza] = useState("");
+
   return (
     <div>
       <h1 className="underline">Hello World</h1>
@@ -8,6 +11,8 @@ const App = () => {
       <div style={{ margin: "10px" }}>
         <PPSelect
           label="Types of Pizza"
+          value={pizza}
+          updateValue={setPizza}
           options={[
             { text: "Pepperoni", value: "pepperoni" },
             { text: "Chicken", value: "chicken" },
---
+++ src/components/basic/select/PPSelect.tsx
@@ -0,0 +1,69 @@
+import { useRef, useState } from "react";
+
+import "./ppSelect.scss";
+import { clickOutside } from "../../../composables/generic";
+
+interface SelectOption {
+  text: string;
+  value: string | number;
+}
+
+interface Props {
+  label: string;
+  options: SelectOption[];
+  value: string;
+  updateValue: React.Dispatch<React.SetStateAction<any>>;
+}
+
+const PPSelect = ({ label, options, value = "", updateValue }: Props) => {
+  // ** Data **
+  const select = useRef<HTMLDivElement | null>(null);
+
+  const [selectActive, setSelectActive] = useState<boolean>(false);
+
+  // ** Methods **
+  document.addEventListener("click", (e: MouseEvent) =>
+    clickOutside(e, select, () => setSelectActive(false))
+  );
+
+  const optionSelected = (option: SelectOption): void => {
+    if (option.text !== value && selectActive) {
+      updateValue(option.value);
+      setSelectActive(false);
+    }
+  };
+
+  return (
+    <div ref={select}>
+      <div className="pp-select">
+        <div
+          className={`pp-select-toggle pp-input pp-input-outlined 
+             ${value ? "pp-input-populated" : ""} ${
+            selectActive ? "pp-input-active" : ""
+          }`}
+          onClick={() => setSelectActive(true)}
+        >
+          <label className={`pp-text-colour-black`}>{label}</label>
+          <span className="pp-select-value">{value}</span>
+
+          {/* select icon */}
+        </div>
+        {selectActive && options.length && (
+          <div className="pp-select-items">
+            {options.map((opt, i) => (
+              <div
+                key={i}
+                className="pp-select-items-item"
+                onClick={() => optionSelected(opt)}
+              >
+                {opt.text}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PPSelect;
