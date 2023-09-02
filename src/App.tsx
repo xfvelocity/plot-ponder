7ffccd4577a19a37c0d8ab22d69dbf50d82a8a15
Alex Long <alexlong2001@outlook.com>
2023-09-02 08:07:07 +0100

    added base for select

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import PPSelect from "./components/select/PPSelect";
+
 const App = () => {
   return (
     <div>
       <h1 className="underline">Hello World</h1>
+
+      <div style={{ margin: "10px" }}>
+        <PPSelect
+          label="Types of Pizza"
+          options={[
+            { text: "Pepperoni", value: "pepperoni" },
+            { text: "Chicken", value: "chicken" },
+          ]}
+        />
+      </div>
     </div>
   );
 };
