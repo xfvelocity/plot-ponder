8490c51a6b3def609745b603fb118bb78fc154ef
Alex Long <alexlong2001@outlook.com>
2023-09-03 15:01:30 +0100

    added more functionality & better styling

--- src/components/basic/slider/PPSlider.tsx
+++ src/components/basic/slider/PPSlider.tsx
@@ -2,37 +2,47 @@ import { useRef, useState } from "react";
 import "./ppSlider.scss";
 
 const PPSlider = () => {
+  // ** Data **
   const sliderElement = useRef<HTMLDivElement | null>(null);
   const selectorElement = useRef<HTMLDivElement | null>(null);
 
   const [selectedNumber, setSelectedNumber] = useState(0);
 
-  const handleMouseMove = (e: MouseEvent): void => {
-    if (!selectorElement.current || !sliderElement.current) return;
-
-    // Calculate the progress made along the bar in decimals 0-10
-    const decimalProgress: number =
-      (e.clientX / sliderElement.current.clientWidth) * 10;
-
-    // Round to the nearest 0.5
-    const rating: string = (Math.round(decimalProgress * 2) / 2).toFixed(2);
-    setSelectedNumber(parseFloat(rating));
-
-    // Get the element with the matching id and move the selector to that position
-    const sliderValue = document.getElementById(rating);
-
-    selectorElement.current.style.left = `${sliderValue?.offsetLeft}px`;
-  };
-
+  // ** Methods **
   const startTracking = (): void => {
     window.addEventListener("mousemove", handleMouseMove);
   };
 
   const stopTracking = (): void => {
     window.removeEventListener("mousemove", handleMouseMove);
+
+    if (selectorElement.current && selectedNumber * 10 >= 100) {
+      // Move the selector left in 5% increments
+      selectorElement.current.style.left = `${selectedNumber * 10}%`;
+    }
   };
 
   window.addEventListener("mouseup", stopTracking);
+
+  const handleMouseMove = (e: MouseEvent): void => {
+    if (!selectorElement.current || !sliderElement.current) return;
+
+    const rect: DOMRect = sliderElement.current.getBoundingClientRect();
+    const moveX: number = e.clientX - rect.left - 10;
+
+    if (sliderElement.current.clientWidth > moveX && moveX >= -10) {
+      // Calculate the progress made along the bar in decimals 0-10
+      const decimalProgress: number =
+        (e.clientX / sliderElement.current.clientWidth) * 10;
+
+      // Round to the nearest 0.5
+      const rating: string = (Math.floor(decimalProgress * 2) / 2).toFixed(2);
+
+      setSelectedNumber(parseFloat(rating));
+
+      selectorElement.current.style.left = `${moveX}px`;
+    }
+  };
 
   return (
     <>
@@ -43,13 +53,18 @@ const PPSlider = () => {
           onMouseDown={startTracking}
           onTouchStart={startTracking}
         />
+      </div>
 
-        {[...Array(21)].map((_, i) => (
-          <div
+      <div className="pp-slider-values">
+        {[...Array(11)].map((_, i) => (
+          <span
             key={i}
-            id={(i / 2).toFixed(2).toString()}
-            className="pp-slider-value"
-          />
+            className={`pp-slider-value ${
+              selectedNumber === i ? "pp-slider-value-selected" : ""
+            }`}
+          >
+            {i}
+          </span>
         ))}
       </div>
 
