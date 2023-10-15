e4f17edcfe49f2664cb615f7d779ede789478575
Alex Long <alexlong2001@outlook.com>
2023-10-15 12:08:15 +0100

    added maskable icon

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,11 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "maskable",
+          },
         ],
       },
     }),
