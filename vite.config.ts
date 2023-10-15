055a6899e5d4ef00a6f1b33cfde2fcfc3b302f2e
Alex Long <alexlong2001@outlook.com>
2023-10-15 12:22:27 +0100

    maskable test

--- vite.config.ts
+++ vite.config.ts
@@ -21,21 +21,18 @@ export default defineConfig({
             src: "pwa-64x64.png",
             sizes: "64x64",
             type: "image/png",
+            purpose: "maskable",
           },
           {
             src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "maskable",
           },
           {
             src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          },
-          {
-            src: "app-icon.svg",
-            sizes: "512x512",
-            type: "image/svg",
             purpose: "maskable",
           },
         ],
