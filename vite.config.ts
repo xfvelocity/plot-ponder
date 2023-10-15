031354bf17676741f90338ec210197850befb83e
Alex Long <alexlong2001@outlook.com>
2023-10-15 11:42:22 +0100

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,41 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Plot Ponder",
+        short_name: "Plot Ponder",
+        display: "standalone",
+        background_color: "#00388B",
+        theme_color: "#00388B",
+        description: "Plot Ponder - Movie reviewer",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
