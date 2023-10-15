0464cbe4c02d449b1029514a2d884016bf37eb05
Alex Long <alexlong2001@outlook.com>
2023-10-15 12:14:00 +0100

    updated icon

--- vite.config.ts
+++ vite.config.ts
@@ -32,11 +32,6 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
-          {
-            src: "maskable-icon-512x512.png",
-            sizes: "512x512",
-            type: "maskable",
-          },
         ],
       },
     }),
